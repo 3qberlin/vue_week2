a963f90b98631bd3a39552fb83b73e3c766861a0
Berlin <3q.berlin@gmail.com>
2024-01-13 15:43:28 +0800

    feat: 🏗️ 調整 助教建議內容

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ const app = createApp({
             axios.post(`${url}v2/admin/signin`, data).then((res) => {
                 const { token, expired } = res.data;
                 document.cookie = `berlinToken=${token}; expires=${new Date(expired)};`;
-                location.href = '?products.html';
+                location.href = 'products.html';
                 console.log('indexToken',token)
             }).catch((err) => {
                 console.log(err.response);
